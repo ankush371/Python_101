fcdcbb9909313322a6def95f15cc96275c1fd194
Ankush kumar <aknukm371@gmail.com>
2026-04-22 21:40:52 +0530

    Password generator

---
+++ Password Generator/main.py
@@ -0,0 +1,22 @@
+def password(password_length):
+    
+    import string
+    import secrets
+   
+    lowercase = string.ascii_lowercase
+    uppercase = string.ascii_uppercase
+    digits = string.digits
+    punctuation = string.punctuation
+    
+
+    all_characters =  uppercase + digits + punctuation + lowercase
+    
+    
+    password = ''.join(secrets.choice(all_characters[::2]) for _ in range(password_length))
+    
+    return password
+
+if __name__ == "__main__":  
+    # Generate a random password of length specified by the user
+    password_length = int(input("Enter the desired password length: "))
+    print("Generated Password:", password(password_length))
